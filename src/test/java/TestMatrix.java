fad020732aabbab550c0dc21a9e9d4d380459e4e
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-10-16 13:38:06 +0200

    Add explicit Matrix#modify test

--- src/test/java/TestMatrix.java
+++ src/test/java/TestMatrix.java
@@ -7,6 +7,31 @@ import java.util.Arrays;
 public class TestMatrix {
 
     @Test
+    public void testModify()
+    {
+        Matrix modLater = new Matrix(
+            new double[][]{
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+            }
+        );
+        modLater.modify(2, 1, 38);
+
+        Matrix modNow = new Matrix(
+            new double[][]{
+                {1, 2, 3, 4},
+                {5, 6, 38, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+            }
+        );
+
+        assert modLater.equals(modNow);
+    }
+
+    @Test
     public void testEquals()
     {
         Matrix matrix = new Matrix();
