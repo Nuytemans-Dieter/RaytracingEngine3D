75fe7c55508f87366e20011c9a1741908808f2d6
Nuytemans-Dieter <nuytemans_dieter@hotmail.com>
2020-11-17 21:16:21 +0100

    Add Matrix test

--- src/test/java/TestMatrix.java
+++ src/test/java/TestMatrix.java
@@ -256,6 +256,17 @@ public class TestMatrix {
         );
 
         assert( expected.equals( mat.multiply(vector) ) );
+
+        Matrix matCopy = new Matrix(
+                new double[][]{
+                        {1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}
+                }
+        );
+
+        assert ( mat.equals( matCopy ) );
     }
 
     @Test
