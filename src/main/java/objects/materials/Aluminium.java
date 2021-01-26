1295d93b9a9abbdb99bc77bfa66c505304d03c7e
Dieter Nuytemans <Nuytemans_Dieter@hotmail.com>
2021-01-26 12:15:34 +0100

    Fix aluminium

--- src/main/java/objects/materials/Aluminium.java
+++ src/main/java/objects/materials/Aluminium.java
@@ -8,15 +8,14 @@ public class Aluminium extends Material {
     public Aluminium()
     {
         super(
-            new Rgb(0.93f, 0.93f, 0.93f),
+            new Rgb(0.45f, 0.45f, 0.45f),
             0.8f,
-            0.2f,
-            0.9f,
+            0.3f,
+            32,
             0.2f,
             1,
             0.0f
         );
-        setEmissive( 0.3f, 0.3f, 0.3f );
     }
 
 }
